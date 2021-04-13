5e1e59ebe8092ffd304200eadba132b877129870
Kristoffer <kristoffersvedal@gmail.com>
2021-04-13 14:32:59 +0200

    Working application window. added module.info

--- src/main/java/idata1002_2021_group11/App.java
+++ src/main/java/idata1002_2021_group11/App.java
@@ -1,21 +1,45 @@
 package idata1002_2021_group11;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+/**
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+ */
+import java.net.URL;
+//import static idata1002_2021_group11.JsonWriteTest.writeJsonSimpleDemo;
 
-import static idata1002_2021_group11.JsonWriteTest.writeJsonSimpleDemo;
+public class App extends Application {
 
-class App {
+  @Override
+  public void start(Stage stage) throws Exception {
+    URL url = getClass().getClassLoader()
+        .getResource("application.fxml");
+    Parent root = FXMLLoader.load(url);
+
+    Scene scene = new Scene(root);
+
+    stage.setTitle("Training app");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+
+
     /**
      * Writing to the file example
-     */
+
     public static void main(String[] args) throws Exception {
         // test to write to file
         writeJsonSimpleDemo("example2.json");
     }
+    */
 
 
     /**
--- src/main/java/idata1002_2021_group11/Controller.java
+++ src/main/java/idata1002_2021_group11/Controller.java
@@ -1,4 +1,7 @@
 package idata1002_2021_group11;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
 }
