a207fe9f3f9b19cd0d66fadf6906888f3e3f4806
Fredrik Hoelseter <fredhoe@stud.ntnu.no>
2021-04-27 13:41:20 +0200

    Added functionality to the calculator view

--- src/main/java/idata1002_2021_group11/ViewCalculatorController.java
+++ src/main/java/idata1002_2021_group11/ViewCalculatorController.java
@@ -7,6 +7,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,12 +19,55 @@ import java.util.ResourceBundle;
 public class ViewCalculatorController implements Initializable {
 
 
+    @FXML
+    private TextField enterRepetitionsField;
+
+    @FXML
+    private TextField enterWeightField;
+
+    @FXML
+    private TextField resultField;
+
+    private Calculator calculator;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        calculator = new Calculator();
     }
 
+    public void calculateMaxLift() {
+        try {
+            float result = calculator.calculateAndSetMaxLift(Float.parseFloat(enterWeightField.getText()),
+                    Integer.parseInt(enterRepetitionsField.getText()));
+            String stringResult = String.valueOf(result);
+            resultField.setText(stringResult);
+        } catch (NumberFormatException numberFormatException) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * Returns to the main menu scene.
+     *
+     * @param event the event
+     * @throws IOException the io exception
+     */
+    @FXML public void returnToMainScene(ActionEvent event) throws IOException
+    {
+        URL createReturnMainMenu = getClass().getClassLoader().getResource("application.fxml");
+
+        assert createReturnMainMenu != null;
+        Parent root = FXMLLoader.load(createReturnMainMenu);
+
+        Scene createReturnMainMenuScene = new Scene(root);
+        Stage createReturnMainMenuWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        root.getStylesheets().add("trainingApp.css");
+        createReturnMainMenuWindow.setScene(createReturnMainMenuScene);
+        createReturnMainMenuWindow.show();
+
+    }
 
 }
