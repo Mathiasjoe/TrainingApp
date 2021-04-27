4698f0a050cf4910f99318358a3327add7ee37bb
Kristoffer <kristoffersvedal@gmail.com>
2021-04-27 14:09:39 +0200

    Added styling and correct text to alert button in calculator window.

--- src/main/java/idata1002_2021_group11/ViewCalculatorController.java
+++ src/main/java/idata1002_2021_group11/ViewCalculatorController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -44,6 +45,14 @@ public class ViewCalculatorController implements Initializable {
             resultField.setText(stringResult);
         } catch (NumberFormatException numberFormatException) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("Insufficient input");
+            alert.setContentText("Input fields can not be empty.\nThey also have to be numeric.");
+
+            //Styling
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.getStylesheets().add("trainingApp.css");;
+            dialogPane.getStyleClass().add("customDialog");
+
             alert.showAndWait();
         }
     }
